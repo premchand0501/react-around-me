0e927178e4230c1014a28329eb58896b4308854b
Prem <premsahni0501@gmail.com>
2019-11-12 12:00:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Swiper from 'react-id-swiper';
+import './assets/scss/App.scss';
+import { continents } from './assets/json/continents';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    swiperInstance: null
+  }
+  render() {
+    const options = {
+      direction: 'vertical',
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      }
+    }
+    const placesOptions = {
+      pagination: {
+        el: '.swiper-places-pagination',
+        type: 'fraction',
+        clickable: true
+      },
+      slidesPerView: 2
+    }
+
+    return (
+      <div className="App container-fluid">
+        <Swiper {...options}>
+          {
+            continents.map((cont, ind) => (
+              <div className="slide" key={ind} style={{
+                backgroundImage: `url(${require(`./assets/img/${cont.image}`)})`,
+              }}>
+                <div className="container content">
+                  <div className="row">
+                    <div className="col col-12 places-swiper">
+                      <Swiper {...placesOptions}>
+                        <div className="places-slide">
+                          <div className="card">
+                            <div className="card-body">
+                              <h1>{cont.name}</h1>
+                              <p>{cont.desc}</p>
+                              <button className="btn btn-success">Explore</button>
+                            </div>
+                          </div>
+                        </div>
+                        {
+                          continents.map((cont, ind) => (
+                            <div className="places-slide" key={ind}>
+                              <div className="card" style={{
+                                backgroundImage: `url(${require(`./assets/img/${cont.image}`)})`,
+                              }}>
+                                <div className="card-body">
+                                  body
+                                </div>
+                              </div>
+                            </div>
+                          ))
+                        }
+                      </Swiper>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </Swiper>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/assets/json/continents.js
@@ -0,0 +1,17 @@
+export const continents = [
+  {
+    name: 'Africa',
+    image: 'africa.png',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat obcaecati voluptate maiores adipisci sapiente animi nam iure sed dignissimos! Molestias vero reiciendis facilis officiis voluptas cum quaerat ad eius soluta.'
+  },
+  {
+    name: 'Asia',
+    image: 'asia.jpg',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat obcaecati voluptate maiores adipisci sapiente animi nam iure sed dignissimos! Molestias vero reiciendis facilis officiis voluptas cum quaerat ad eius soluta.'
+  },
+  {
+    name: 'Australia',
+    image: 'australia.jpg',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat obcaecati voluptate maiores adipisci sapiente animi nam iure sed dignissimos! Molestias vero reiciendis facilis officiis voluptas cum quaerat ad eius soluta.'
+  }
+];
